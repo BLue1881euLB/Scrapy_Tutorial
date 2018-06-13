08dbf0335707ebcbe3b17b6d1ce291dcb2400e12
2412763915 <2412763915@qq.com>
2018-06-13 10:15:33 +0800

    Test

--- matplotlib_examples/matplotlib_examples/pipelines.py
+++ matplotlib_examples/matplotlib_examples/pipelines.py
@@ -4,9 +4,7 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-from scrapy.pipelines.files import FilesPipeline
-from urllib.parse import  urlparse
-from os.path import basename,dirname,join
+
 
 from scrapy.pipelines.files import  FilesPipeline
 from urllib.parse import urlparse
